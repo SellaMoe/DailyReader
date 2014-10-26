8df904a4e8bfe906561beb2e63f0d95b619d08b3
lmmloli <limingming414@gmail.com>
2014-10-26 21:06:42 +0800

    新建工程
    新建团队DailyReader工程

---
+++ DailyReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DailyReader
+//
+//  Created by limingming on 14/10/26.
+//  Copyright (c) 2014年 SellaMoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
